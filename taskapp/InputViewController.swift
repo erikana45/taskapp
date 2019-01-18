21095a2779a4de7da3c09f5117cf6309949fee6d
勝木えり <r8ea2288e777y@softbank.ne.jp>
2019-01-18 14:13:18 +0900

    Realmでタスク保存

--- taskapp/InputViewController.swift
+++ taskapp/InputViewController.swift
@@ -7,21 +7,50 @@
 //
 
 import UIKit
+import RealmSwift
 
-class InputViewController: UIViewController {
-
+class InputViewController: UIViewController  {
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var contentsTextView: UITextView!
     @IBOutlet weak var datePicker: UIDatePicker!
+    
+    let realm = try! Realm()    // 追加する
+    var task: Task!   // 追加する
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // 背景をタップしたらdismissKeyboardメソッドを呼ぶように設定する
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target:self, action:#selector(dismissKeyboard))
+        self.view.addGestureRecognizer(tapGesture)
+        
+        titleTextField.text = task.title
+        contentsTextView.text = task.contents
+        datePicker.date = task.date
+    }
+    
+    @objc func dismissKeyboard(){
+        // キーボードを閉じる
+        view.endEditing(true)
+    }
+
+    // 追加する
+    override func viewWillDisappear(_ animated: Bool) {
+        try! realm.write {
+            self.task.title = self.titleTextField.text!
+            self.task.contents = self.contentsTextView.text
+            self.task.date = self.datePicker.date
+            self.realm.add(self.task, update: true)
+        }
+        
+        super.viewWillDisappear(animated)
     }
     
 
+    
+    
+    
     /*
     // MARK: - Navigation
 
